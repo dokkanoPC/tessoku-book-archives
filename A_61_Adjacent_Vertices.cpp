690d4815039058f5087761ec2c53d3e41d0e1233
dokkanoPC <2020marumaria@gmail.com>
2026-03-21 00:08:45 +0900

    add A61 - Adjacent Vertices

---
+++ A_61_Adjacent_Vertices.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric> // std::iota()
+#include <queue>
+#include <functional>
+void Show(const std::vector<int> &v)
+{
+    std::cout << ": {";
+
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        std::cout << v[i] + 1;
+
+        if (i != v.size() - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+
+    std::cout << "}\n";
+}
+/*struct Edge {方向あり
+    int from;
+    int to;
+    int cost;//重みあり
+ };*/
+int main()
+{
+    int N, M, a, b;
+    std::cin >> N >> M;
+    std::vector<std::vector<int>> G(N);
+    while (M--)
+    {
+        std::cin >> a >> b;
+        G.at(a - 1).push_back(b - 1);
+        G.at(b - 1).push_back(a - 1); // 方向なし
+    }
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << i + 1;
+        Show(G.at(i));
+    }
+}
